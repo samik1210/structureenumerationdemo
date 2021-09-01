76197ff65a1e6963b98716472397a9a76ac5f874
parag <parag@DESKTOP-I2Q9QKQ>
2021-09-01 13:53:07 +0530

    Add project files.

---
+++ structuredemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace structuredemo
+{
+            public struct Person
+        {
+         
+            public string Name;
+            public int Age;
+            public int Weight;
+
+        }
+
+        class samu1
+        {
+
+            static void Main(string[] args)
+            {
+
+                Person P1;
+                P1.Name = "samiksha parag";
+                P1.Age = 23;
+                P1.Weight = 90;
+                Console.WriteLine("Data Stored in P1 is " +
+                                   P1.Name + ", age is " +
+                                   P1.Age + " and weight is " +
+                                   P1.Weight);
+
+            }
+        }
+
+}
+
+    
---
+++ structureenumerationdemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace structureenumerationdemo
+{
+            enum month
+        {
+
+       
+            jan,
+            feb,
+            mar,
+            apr,
+            may
+
+        }
+
+        class Program
+        { 
+            static void Main(string[] args)
+            {
+                Console.WriteLine("The value of jan in month " +
+                                  "enum is " + (int)month.jan);
+                Console.WriteLine("The value of feb in month " +
+                                  "enum is " + (int)month.feb);
+                Console.WriteLine("The value of mar in month " +
+                                  "enum is " + (int)month.mar);
+                Console.WriteLine("The value of apr in month " +
+                                  "enum is " + (int)month.apr);
+                Console.WriteLine("The value of may in month " +
+                                  "enum is " + (int)month.may);
+            }
+        }
+      
+        
+    
+}
+
